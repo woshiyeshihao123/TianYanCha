ee8ec4733f95633a14028443b16b88a268d2c984
yeshihao <706722153@qq.com>
2018-06-07 09:08:45 +0800

    1

--- MyWork/vue-demo-cnodejs/config/index.js
+++ MyWork/vue-demo-cnodejs/config/index.js
@@ -56,13 +56,13 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/dist/',
 
     /**
      * Source Maps
      */
 
-    productionSourceMap: true,
+    productionSourceMap: false,
     // https://webpack.js.org/configuration/devtool/#production
     devtool: '#source-map',
 
--- MyWork/vue-demo-cnodejs/src/main.js
+++ MyWork/vue-demo-cnodejs/src/main.js
@@ -6,8 +6,12 @@ import router from './router'
 
 // 引用API文件
 import api from './api/index.js'
+// 引用工具文件
+import utils from './utils/index.js'
 // 将API方法绑定到全局
 Vue.prototype.$api = api
+// 将工具方法绑定到全局
+Vue.prototype.$utils = utils
 
 Vue.config.productionTip = false
 
